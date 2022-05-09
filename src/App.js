8c0e50a7d8d093e5de3fe937e5ce66a05bc3c1eb
varun Pratap <varun.singh01@indiamart.com>
2022-05-09 12:40:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,26 +6,52 @@ import './App.css';
 function App() {
  
   const [Movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-  const moviesFetchHandler = () => {
+  // 1st way of fetcing the data from api
+
+  // const moviesFetchHandler = () => {
     
-    fetch('https://swapi.py4e.com/api/films/').then((data) => {
+  //   fetch('https://swapi.py4e.com/api/films/').then((data) => {
 
-      return data.json();
+  //     return data.json();
       
-    }).then((data) => {
+  //   }).then((data) => {
 
-      const transfomedMovieData = data.results.map(movies => {
-        return {
-          id: movies.episode_id,
-          title: movies.title,
-          releaseDate: movies.release_date,
-          openingText: movies.opening_crawl
-        }
-      })
-      setMovies(transfomedMovieData);
-    })
+  //     const transfomedMovieData = data.results.map(movies => {
+  //       return {
+  //         id: movies.episode_id,
+  //         title: movies.title,
+  //         releaseDate: movies.release_date,
+  //         openingText: movies.opening_crawl
+  //       }
+  //     })
+  //     setMovies(transfomedMovieData);
+  //   })
+  // }
+
+  // 2nd way of fetching data from api
+
+  async function moviesFetchHandler (){
+    
+    setLoading(true);
+    const response = await fetch('https://swapi.py4e.com/api/films/');
+
+    const data = await response.json();
+
+    const transfomedMovieData = data.results.map(movie => (
+      {
+        id: movie.episode_id,
+        title: movie.title,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl
+      }
+    ))
+
+    setMovies(transfomedMovieData);
+    setLoading(false);
   }
+
 
   return (
     <React.Fragment>
@@ -33,7 +59,9 @@ function App() {
         <button onClick={moviesFetchHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={Movies} />
+        {!isLoading && Movies.length > 0 && <MoviesList movies={Movies} />}
+        {!isLoading && Movies.length === 0 && <p>No Movies found..</p>}
+        {isLoading && <p>Loading ...</p>}
       </section>
     </React.Fragment>
   );
